ea64aadfdb82ce43bfba40eef97ef9f6df86be97
Kylenn Huang <79785468+kylenn-huang@users.noreply.github.com>
2022-06-13 00:51:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-// Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
-var name = 'hello';
-// TODO write your code here...
 
+        // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
+        var name = 'hello';
+        console.log('name', name.toUpperCase());
 
-// Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
-var sentence = 'good afternoon, mr mike.';
-//TODO write your code here...
+        // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
+        var sentence = 'good afternoon, mr mike.';
+        var reg = /(\s?)(\w+)/g
+        var res = sentence.replace(reg, function (match, space, letter) {
+            return space + letter[0].toUpperCase() + letter.substr(1);
+        });
+        console.log('res', res);
 
+        // Complete the following procedure to extract the amount of money variable, output: 20.
+        var money = '￥20';
+        var moneyCount = money.substring(1)
+        console.log('money', moneyCount);
 
-// Complete the following procedure to extract the amount of money variable, output: 20.
-var money = '￥20';
-//TODO write your code here...
